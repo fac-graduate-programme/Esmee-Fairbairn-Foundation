fe5b24c801229f88f0f749d6ef8df7ee4abed9da
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-09-09 15:25:30 +0300

    edit the button icon

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -1,16 +1,29 @@
-import React from "react"
+import React from "react";
 
-import "./style.css"
+import {
+  faArrowLeft,
+  faArrowRight,
+  faCheckCircle
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import "./style.css";
 
 export default function Button({
   children,
   onClick = undefined,
   className = "",
   type = "button",
+  leftIcon = undefined,
+  done = undefined,
+  rightIcon = undefined
 }) {
   return (
-    <button className={`btn ${className}`} type={type} onClick={onClick} >
+    <button className={`btn ${className}`} type={type} onClick={onClick}>
+      {leftIcon ? <FontAwesomeIcon icon={faArrowLeft} /> : null}
       {children}
+      {done ? <FontAwesomeIcon icon={faCheckCircle} /> : null}
+      {rightIcon ? <FontAwesomeIcon icon={faArrowRight} /> : null}
     </button>
   );
 }
