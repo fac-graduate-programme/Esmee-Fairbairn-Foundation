d15731106ec270e22d0202a33764feadc6d11f90
unknown <sallamtanna2015@hotmail.com>
2019-09-09 15:37:58 +0300

    using destructive

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -8,12 +8,13 @@ import {
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Button(props) {
+  const { label, style, done, rightIcon, leftIcon, onClick } = props;
   return (
-        <button className={classes.button} style={props.style} onClick={props.onClick}>
-          {props.leftIcon? <FontAwesomeIcon icon={faArrowLeft}/>: null}
-            <span>{props.label}</span>
-          {props.done? <FontAwesomeIcon icon={faCheckCircle}/>: null}
-          {props.rightIcon? <FontAwesomeIcon icon={faArrowRight}/>: null}
+        <button className={classes.button} style={style} onClick={onClick}>
+          {leftIcon? <FontAwesomeIcon icon={faArrowLeft}/>: null}
+            <span>{label}</span>
+          {done? <FontAwesomeIcon icon={faCheckCircle}/>: null}
+          {rightIcon? <FontAwesomeIcon icon={faArrowRight}/>: null}
         </button>
   )
 }
