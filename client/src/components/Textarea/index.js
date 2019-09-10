235837ac9bfe9cfdcefaeb36bfd53aa338720b58
unknown <sallamtanna2015@hotmail.com>
2019-09-10 09:19:04 +0300

    Edit style

--- client/src/components/Textarea/index.js
+++ client/src/components/Textarea/index.js
@@ -6,7 +6,7 @@ export default function Textarea(props) {
   const { wordsLimit, onChange } = props;
   return (
     <container className='textarea'>
-      <p className='textarea-limitation'>use up to {wordsLimit} words...</p>
+      <p className='textarea-limitation'>Use up to {wordsLimit} words...</p>
       <textarea className='textarea-answer' placeholder='Type here...' onChange={onChange} />
     </container>
   )
