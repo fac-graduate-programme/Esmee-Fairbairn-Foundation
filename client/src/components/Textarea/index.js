eadde97c635776e493bcc12a2b579e0d32f8a125
unknown <sallamtanna2015@hotmail.com>
2019-09-10 09:20:13 +0300

    Some editings

--- client/src/components/Textarea/index.js
+++ client/src/components/Textarea/index.js
@@ -3,7 +3,9 @@ import React from 'react';
 import './style.css'
 
 export default function Textarea(props) {
+
   const { wordsLimit, onChange } = props;
+  
   return (
     <container className='textarea'>
       <p className='textarea-limitation'>Use up to {wordsLimit} words...</p>
