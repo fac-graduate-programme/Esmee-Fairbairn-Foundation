db22da176b6540297945b9845725495706c8df98
dalmano <dalmano@outlook.com>
2019-09-10 15:13:34 +0100

    Changed model to material ui Relates #31

--- client/src/components/PopUp/index.js
+++ client/src/components/PopUp/index.js
@@ -1,17 +1,60 @@
 import React from "react";
 import "./style.css";
-import { Modal, Button } from "react-bootstrap";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    textAlign: "center"
+  }
+}));
 
 const PopUp = props => {
+  const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <Modal {...props}>
-      <Modal.Body>
-        <p>{props.content}</p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Ok</Button>
-      </Modal.Footer>
-    </Modal>
+    <div>
+      <button type="button" onClick={handleOpen}>
+        Click me
+      </button>
+      <Modal
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+        open={open}
+        onClose={handleClose}
+        {...props}
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <p>{props.content}</p>
+        </div>
+      </Modal>
+    </div>
   );
 };
 
