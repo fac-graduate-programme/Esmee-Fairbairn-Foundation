08082317778eab020b46039136772acfe02bbd91
unknown <sallamtanna2015@hotmail.com>
2019-09-10 10:42:14 +0300

    Some modifications

--- client/src/components/Review/index.js
+++ client/src/components/Review/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React from 'react'
 
 import './style.css'
 
 export default function Review(props) {
+
   const { href, answer1, answer2, answer3 } = props;
+
   return (
-    <container className='review'>
-      <div className='review-guidelines'><h2>Refer to <strong><a href={href} className='review-guidelines-href'>application guidelines</a></strong></h2></div>
+    <div className='review'>
+      <div className='review-guidelines'>
+        <h2>Refer to
+          <strong>
+            <a href={href} className='review-guidelines-href'>
+              application guidelines
+            </a>
+          </strong>
+        </h2>
+      </div>
       <div>
         <h1 className='review-question'>What is the change you are aiming to achieve?</h1>
         <p className='review-answer'>{answer1}</p>
@@ -21,6 +31,6 @@ export default function Review(props) {
         <h1 className='review-question'>A brief description of your organisation?</h1>
         <p className='review-answer'>{answer3}</p>
       </div>
-    </container>
+    </div>
   );
 }
