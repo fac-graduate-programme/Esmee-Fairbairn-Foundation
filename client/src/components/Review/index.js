12f2c40fcd0a3b1cba3595e90db6a21ba8eed5c0
unknown <sallamtanna2015@hotmail.com>
2019-09-10 11:13:15 +0300

    Convert static question to prop question

--- client/src/components/Review/index.js
+++ client/src/components/Review/index.js
@@ -4,7 +4,7 @@ import './style.css'
 
 export default function Review(props) {
 
-  const { href, answer1, answer2, answer3 } = props;
+  const { question1, question2, question3, href, answer1, answer2, answer3 } = props;
 
   return (
     <div className='review'>
@@ -18,17 +18,17 @@ export default function Review(props) {
         </h2>
       </div>
       <div>
-        <h1 className='review-question'>What is the change you are aiming to achieve?</h1>
+        <h1 className='review-question'>{question1}</h1>
         <p className='review-answer'>{answer1}</p>
       </div>
 
       <div>
-        <h1 className='review-question'>What work will you deliver with the grant?</h1>
+        <h1 className='review-question'>{question2}</h1>
         <p className='review-answer'>{answer2}</p>
       </div>
 
       <div>
-        <h1 className='review-question'>A brief description of your organisation?</h1>
+        <h1 className='review-question'>{question3}</h1>
         <p className='review-answer'>{answer3}</p>
       </div>
     </div>
