635a55603f65eabea637c93d4e49fb15d9f48296
unknown <sallamtanna2015@hotmail.com>
2019-09-11 12:12:34 +0300

    Rename the function

--- client/src/pages/Questions/index.js
+++ client/src/pages/Questions/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import Tabs from './components/Tabs'
 
-export default function QuestionsPage(props) {
+export default function Page4(props) {
 
   return (
     <Tabs />
