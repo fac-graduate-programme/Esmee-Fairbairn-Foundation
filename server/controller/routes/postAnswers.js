60480139238cd31b9d54192d852eed0c77856c46
dubhcait <dubhcait@live.ie>
2019-09-16 11:31:48 +0100

    change error handling

--- server/controller/routes/postAnswers.js
+++ server/controller/routes/postAnswers.js
@@ -2,23 +2,20 @@ const addAnswer = require("../../db/queries/postAnswers");
 
 module.exports = (req, res, next) => {
   try {
+
     // this is if we send all the answers as a object in the post body
     // the structure needs to be { answer1, answer2, answer3 } = req.body.data;
 
     const preValidatedAnswers = req.body.data;
 
     addAnswer(preValidatedAnswers).then(result => {
-      // addAnswers returns the id which is auto generated int - this means the SQL database hase processed correctly
+        // addAnswers returns the id which is auto generated int - this means the SQL database hase processed correctly 
       if (Number.isInteger(result.rows[0].id)) {
         res.send({
           data: "ok",
           statusCode: 200
         });
-      } else
-        throw res.send({
-          error: true,
-          statusCode: 500
-        });
+      } else throw new Error('there is an error in regards addAnswer query');
     });
   } catch (err) {
     next(err);
