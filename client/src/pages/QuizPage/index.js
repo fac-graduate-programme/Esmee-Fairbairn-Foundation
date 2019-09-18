1f8decdf09551a54e93d2e3ade8677afa0f801b7
unknown <sallamtanna2015@hotmail.com>
2019-09-18 23:06:41 +0300

    Add more checks

--- client/src/pages/QuizPage/index.js
+++ client/src/pages/QuizPage/index.js
@@ -1,33 +1,54 @@
 import React, { useState } from 'react'
-
+import Swal from 'sweetalert2'
 import RadioGroup from '@material-ui/core/RadioGroup'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import Radio from '@material-ui/core/Radio'
-import { makeStyles } from '@material-ui/core/styles';
 
 import Button from '../../components/Button'
 
 import './style.css'
 
-
-
  function QuizPage (props) {
 
-    const [trusteesValue, setTrusteesValue] = useState('female');
-    const [charityValue, setCharityValue] = useState('female');
-
+    const [trusteesValue, setTrusteesValue] = useState()
+    const [charityValue, setCharityValue] = useState()
+    const [annualTurnover, setAnnualTurnover] = useState()
 
     const  handleTrusteesChange = (event) => {
-        setTrusteesValue(event.target.value);
+      setTrusteesValue(event.target.value);
     }
 
     const  handleCharityChange = (event) => {
-        setCharityValue(event.target.value);
+      setCharityValue(event.target.value);
+    }
+
+    const handleAnnualTurnover = (event) => {
+      setAnnualTurnover(event.target.value)
+    }
+
+    const handleSubmit = () => {
+      if(!trusteesValue || !charityValue || !annualTurnover) {
+        Swal.fire({
+          type: 'error',
+          title: 'Oops...',
+          text: 'Please fill all the questions!'
+        })
       }
+      else {
+        if(isNaN(annualTurnover)) {
+          Swal.fire({
+          type: 'error',
+          title: 'Oops...',
+          text: 'Annual Turnover should be a number!'
+        })} else {
+          // go to fil the application
+        }
+      }
+    }
 
     return (<div className='page'>
         <p>1. What is you annual turnover?</p>
-        <input type='text' className='page-annualTurnover' />
+        <input type='text' className='page-annualTurnover' placeholder='Enter a value' onChange={handleAnnualTurnover} />
         <p>2. Do you have a minimum of 3 trustees?</p>   
         <RadioGroup aria-label="trustees" name="trustees" value={trusteesValue} onChange={handleTrusteesChange}>
           <FormControlLabel value="trusteesYes" control={<Radio />} label="Yes" />
@@ -40,7 +61,7 @@ import './style.css'
           <FormControlLabel value="charityNo" control={<Radio />} label="No" />
         </RadioGroup>
 
-        <Button className='page-submit'>submit</Button>
+        <Button className='page-submit' onClick={handleSubmit}>submit</Button>
     </div>)
 }
 
