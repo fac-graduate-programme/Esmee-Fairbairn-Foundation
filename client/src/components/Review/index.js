4c9e62a476fba3996e944ddc986646b9eed44e61
unknown <sallamtanna2015@hotmail.com>
2019-09-20 23:15:43 +0300

    Remove link from Review component

--- client/src/components/Review/index.js
+++ client/src/components/Review/index.js
@@ -8,16 +8,6 @@ export default function Review(props) {
 
   return (
     <div className='review'>
-      <div className='review-guidelines'>
-        <h2>Refer to
-          <strong>
-            <a href={href} className='review-guidelines-href'>
-              application guidelines
-            </a>
-          </strong>
-        </h2>
-      </div>
-
       <div>
         {
           questions.map((question, index) => <div key={question.id}>
