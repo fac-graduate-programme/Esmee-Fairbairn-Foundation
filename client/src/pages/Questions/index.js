029ac1dada7c7423b6c987bebb291ab9015afb1e
unknown <sallamtanna2015@hotmail.com>
2019-09-20 23:06:10 +0300

    Send the whole question object as prop to TabBody component

--- client/src/pages/Questions/index.js
+++ client/src/pages/Questions/index.js
@@ -150,10 +150,7 @@ const Page4 = props => {
         {questions.slice(0,3).map((question, index) => {
               return(<TabPanel key={question.id} value={value} index={index} dir={theme.direction}>
               <TabBody
-                tips = {question.tips}
-                question = {question.title}
-                expectations = {question.expectations}
-                wordsLimit= {question.wordsLimit}
+                question = {question}
                 onChangeAnswer={e => setAnswer(index+1, e.target.value)}
               />
               <div className='tabs'>
