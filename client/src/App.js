62caac1ef4d35d4b983f7979f138ca642dddd40d
unknown <sallamtanna2015@hotmail.com>
2019-09-25 13:09:42 +0300

    rename the pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Swal from 'sweetalert2'
 
-import Page1 from './pages/Welcome'
-import Page2 from './pages/Note'
-import Page3 from './pages/Tips'
-import Page4 from './pages/Questions'
+import WelcomePage from './pages/Welcome'
+import NotePage from './pages/Note'
+import TipsPage from './pages/Tips'
+import QuestionsPage from './pages/Questions'
 import QuizPage from './pages/QuizPage'
 import logo from './assets/title-logo.svg'
 
@@ -55,15 +55,15 @@ const App = () => {
       
      {showQuiz == true ? <QuizPage result={handleQuizResult} /> : null}
      {showApp  == true ? <div>
-      {pageNumber === 1 ? <Page1 handleStaticButton = {handleStaticButton}  /> : null}
-      {pageNumber === 2 ? <Page2 handleStaticButton = {handleStaticButton}  /> : null}
-      {pageNumber === 3 ? <Page3 handleStaticButton = {handleStaticButton}  /> : null}
+      {pageNumber === 1 ? <WelcomePage handleStaticButton = {handleStaticButton}  /> : null}
+      {pageNumber === 2 ? <NotePage handleStaticButton = {handleStaticButton}  /> : null}
+      {pageNumber === 3 ? <TipsPage handleStaticButton = {handleStaticButton}  /> : null}
       {pageNumber === 4 ? error ?  Swal.fire({
           type: 'error',
           title: 'Oops...',
           text: 'Something went wrong, try again!',
         }) 
-        : <Page4 questions={questions}/> : null}
+        : <QuestionsPage questions={questions}/> : null}
       </div> : null}
   </div>
   )
